84ace2b9b1715229e56ff69b439381dca0b59dd3
Karim <kfmust@gmail.com>
2024-09-23 18:17:06 +0300

    Home Screen Status Bar Inset

--- app/src/main/java/com/mustafin/coffee/ui/screens/home/HomeScreenFragment.java
+++ app/src/main/java/com/mustafin/coffee/ui/screens/home/HomeScreenFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,17 +35,29 @@ public class HomeScreenFragment extends Fragment {
             new ShortCoffeeModel("Flat White", "Espresso", 3.53),
             new ShortCoffeeModel("Caffe Mocha", "Deep Foam", 4.53),
             new ShortCoffeeModel("Flat White", "Espresso", 3.53)
-            );
+    );
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.home_screen, container, false);
 
+        LinearLayout header = root.findViewById(R.id.home_header);
+        ViewCompat.setOnApplyWindowInsetsListener(header, (view, insets) -> {
+            int statusBarInset = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top;
+            view.setPadding(
+                    view.getPaddingLeft(),
+                    statusBarInset,
+                    view.getPaddingRight(),
+                    view.getPaddingBottom()
+            );
+            return insets;
+        });
+
         RecyclerView coffeeListView = root.findViewById(R.id.coffee_recycler_view);
         coffeeListView.setAdapter(new CoffeeListAdapter(coffeeList));
         coffeeListView.setLayoutManager(new GridLayoutManager(getContext(), 2));
-        int spaceWidthInPx = (int)(16 * this.getResources().getDisplayMetrics().density);
+        int spaceWidthInPx = (int) (16 * this.getResources().getDisplayMetrics().density);
         coffeeListView.addItemDecoration(new CoffeeListDecoration(spaceWidthInPx));
 
         return root;
